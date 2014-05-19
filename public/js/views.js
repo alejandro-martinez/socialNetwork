da30d77dacc8473d485455f14470047250a33d7f
Alejandro <alejandromartinezap@gmail.com>
2014-05-19 18:27:32 -0300

    fix error eventos next y prev

--- public/js/views.js
+++ public/js/views.js
@@ -95,8 +95,8 @@ var Views = {
 	NewsFeed: Backbone.View.extend({
 		el: $("#content"),
 		events: {
-			'click .nextPage' : 'nextPage',
-			'click .prevPage'	: 'prevPage'
+			'click a.newsFeed.nextPage' : 'nextPage',
+			'click a.newsFeed.prevPage'	: 'prevPage'
 		},
 		initialize: function(){
 			this.render();
@@ -124,7 +124,11 @@ var Views = {
     	}
 	}),
 	Wall: Backbone.View.extend({
-		el: $("#wall"),
+		el: $("body"),
+		events: {
+			'click a.wall.nextPage' : 'nextPage',
+			'click a.wall.prevPage'	: 'prevPage'
+		},
 		initialize: function(){
 			this.render();
 		},
@@ -287,34 +291,8 @@ var Views = {
 					friend: This.options.friendInfo,
 					wall: wallUpdates.models
 				}));
-				console.log(This.options.friendInfo)
-				console.log(wallUpdates.models)
 			});
 		},
-	}),
-	Paginator: Backbone.View.extend({
-
-	    className: "pagination pagination-centered",
-
-	    initialize:function () {
-	        this.model.bind("reset", this.render, this);
-	        this.render();
-	    },
-
-	    render:function () {
-
-	        var items = this.model.models;
-	        var len = items.length;
-	        var pageCount = Math.ceil(len / 8);
-
-	        $(this.el).html('<ul />');
-
-	        for (var i=0; i < pageCount; i++) {
-	            $('ul', this.el).append("<li" + ((i + 1) === this.options.page ? " class='active'" : "") + "><a href='#wines/page/"+(i+1)+"'>" + (i+1) + "</a></li>");
-	        }
-
-	        return this;
-	    }
 	})
 
 }
