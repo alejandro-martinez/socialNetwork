07ad2771e65ee054072b8deabd6bc43d2cc4bc21
sofipacifico <spacifico@makingsense.com>
2014-11-06 00:35:26 -0300

    nav-user, added!

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,25 @@
 /* Router App ---------------------------- */
 var AppRouter = Backbone.Router.extend({
     routes: {
-		"fbid/:fbid"			 : "index",
-		"albums/:fbid"			 : "albums",
-		"albums/:albumid/photos" : "albumPhotos",
+        "fbid/:fbid"             : "index",
+        "albums/:fbid"           : "albums",
+        "albums/:albumid/photos" : "albumPhotos",
         "updateAlbum/:albumid"   : "updateAlbum",
-		"photos/:fbid"			 : "photos",
+        "photos/:fbid"           : "photos",
         "photos/upload"          : "uploadPhoto",
         "friendPhotos/:id"       : "friendPhotos",
-		"posts/:fbid"			 : "posts",
-		"post/:postid"			 : "post",
-		"friends/:fbid"			 : "friends",
+        "posts/:fbid"            : "posts",
+        "post/:postid"           : "post",
+        "friends/:fbid"          : "friends",
         "friend/:id"             : "friendProfile",
         "friendAdd/:id"          : "friendAdd",
         "groupsList"             : "groupsList",
         "groupFeed/:id"          : "groupFeed",
     },
     initialize: function (appC) {
-    	this.api = appC.ac;    									//Objeto AppController 
-    	this.data = appC.ac.dataDefault;						//Info de usuario logueado
-    	this.loggedUser = appC.ac.loggedUser;
+        this.api = appC.ac;                                     //Objeto AppController 
+        this.data = appC.ac.dataDefault;                        //Info de usuario logueado
+        this.loggedUser = appC.ac.loggedUser;
         This = this;
 
         // instancio y actualizo por primera vez las colecciones criticas
@@ -38,44 +38,49 @@ var AppRouter = Backbone.Router.extend({
     },
     index: function(){
         var This = this;
-    	if (this.loggedUser) {
-    			this.mainView = new Views.Main({api: this.api, model: this.data});					//Menu lateral
+        if (this.loggedUser) {
+                this.mainView = new Views.Main({api: this.api, model: this.data});                  //Menu lateral
                 this.api.newsFeed(function(response){
-			    	if(window.location.hash.split('/')[0] == "#fbid") {
-	                    this.newsFeed = new Views.NewsFeed({model: response, api: This.api});
+                    if(window.location.hash.split('/')[0] == "#fbid") {
+                        this.newsFeed = new Views.NewsFeed({model: response, api: This.api});
                         this.newPostView = new Views.NewPost({api: This.api});                  //Que estas pensando
                         this.searchView = new Views.Search();                                       //Buscador
-			    	}
+                    }
                 });
-		}
-		else {
-			new Views.NotLoggedInView();						//Muestra logo de Facebook +
-		}
-		new Views.Header({model: this.data,api: this.api});					//Barra superior con los datos del usuario
+        }
+        else {
+            new Views.NotLoggedInView();                        //Muestra logo de Facebook +
+        }
+        new Views.Header({model: this.data,api: this.api});                 //Barra superior con los datos del usuario
+        selectMenu('nav-user')
     },
     albumPhotos: function(id){
         var This = this;
         this.api.getAlbumPhotos(id,9,function(response){
            this.albumsPhotoView = new Views.albumPhotos({model: response, api: This.api});
         });   
+        selectMenu('nav-albums')
     },
     albums: function(id){
         var This = this;
         this.api.getAlbums(id,function(response){
            this.albumsView = new Views.Albums({model: response, api: This.api, friendId: id});
         });   
+        selectMenu('nav-albums')
     },
     updateAlbum: function(id){
         var This = this;
         this.api.getAlbumPhotos(id,9,function(response){
            this.UpdateAlbumView = new Views.UpdateAlbum({model: response,albumId: id, api: This.api});
         });   
+        selectMenu('nav-albums')
     },
     friends: function(){
         var This = this;
         this.api.getFriends(function(response){
             this.friendsView = new Views.Friends({api: This.api,model: response}); 
         });
+        selectMenu('nav-friends')
     },
     friendProfile: function(id){
         var This = this;
@@ -87,6 +92,7 @@ var AppRouter = Backbone.Router.extend({
                 This.friendView = new Views.friendProfile({wall: friendWall, friendInfo: friendInfo, amigo: This.esAmigo, api: This.api}); 
             });
         });
+        selectMenu('nav-friends')
     },
     friendAdd: function(id){
         this.api.addFriend(id);
@@ -97,23 +103,27 @@ var AppRouter = Backbone.Router.extend({
             This.wallView = new Views.Wall({model: response, api: This.api});    
             This.newPostView = new Views.NewPost({api: This.api});                                             //Que estas pensando
         });
+        selectMenu('nav-posts')
     },
     photos: function(){
         var This = this;
         this.api.getPhotos(function(response){
             this.photos = new Views.Photos({model: response,api: This.api});
         });
+        selectMenu('nav-photos')
     },
     friendPhotos: function(id){
         var This = this;
         this.api.getFriendPhotos(id,function(response){
             this.friendPhotos = new Views.friendPhotos({model: response,api: This.api});
         });
+        selectMenu('nav-photos')
     },
     groupsList: function(){
         this.api.getUserGroups(function(response){
             this.groupsView = new Views.Groups({model: response});
         });
+        selectMenu('nav-groups')
     },
     groupFeed: function(id){
         var This = this;
@@ -121,5 +131,11 @@ var AppRouter = Backbone.Router.extend({
             This.groupFeedView = new Views.GroupFeed({model: response, group_id:id, api: This.api});
             This.newGroupPostView = new Views.NewPost({api: This.api});
         });
+        selectMenu('nav-groups')
     },
 });
+
+var selectMenu = function(op){
+    $('.nav-button').removeClass("active");
+    $('#' + op).addClass("active");
+}
